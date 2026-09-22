55b295dc1abac38e9d3af8023494c993858743d3
g <g@g.g>
2026-09-22 21:03:46 +0200

    Include Matematyka Chapter 8 in Playground build

--- scripts/sync-apps.ps1
+++ scripts/sync-apps.ps1
@@ -104,7 +104,7 @@ $allowlists = [ordered]@{
     "Chapter1/index.html", "Chapter1/game.js", "Chapter2/index.html", "Chapter2/game.js",
     "Chapter3/index.html", "Chapter3/game.js", "Chapter4/index.html", "Chapter4/game.js",
     "Chapter5/index.html", "Chapter5/game.js", "Chapter6/index.html", "Chapter6/game.js",
-    "Chapter7/index.html", "Chapter7/game.js"
+    "Chapter7/index.html", "Chapter7/game.js", "Chapter8/index.html", "Chapter8/game.js"
   ) }
 }
 
@@ -122,7 +122,7 @@ try {
 
   Get-ChildItem -LiteralPath $stagingRoot -Recurse -Filter index.html | ForEach-Object {
     $relative = [IO.Path]::GetRelativePath($stagingRoot, $_.FullName).Replace('\', '/')
-    Rewrite-VendoredHtml $_.FullName ($relative -match '^matematyka/Chapter[1-7]/index\.html$')
+    Rewrite-VendoredHtml $_.FullName ($relative -match '^matematyka/Chapter[1-8]/index\.html$')
   }
 
   $forbidden = Get-ChildItem -LiteralPath $stagingRoot -Recurse -File | Where-Object {
@@ -162,6 +162,7 @@ try {
     @("apps/matematyka/Chapter5/", "apps/matematyka/Chapter5/index.html"),
     @("apps/matematyka/Chapter6/", "apps/matematyka/Chapter6/index.html"),
     @("apps/matematyka/Chapter7/", "apps/matematyka/Chapter7/index.html"),
+    @("apps/matematyka/Chapter8/", "apps/matematyka/Chapter8/index.html"),
     @("apps/matematyka/", "apps/matematyka/index.html"), @("apps/minesweeper/", "apps/minesweeper/index.html"),
     @("apps/blockfall/", "apps/blockfall/index.html"), @("apps/15puzzle/", "apps/15puzzle/index.html"),
     @("apps/sudoku/", "apps/sudoku/index.html"), @("apps/2048/", "apps/2048/index.html"), @("", "index.html")
